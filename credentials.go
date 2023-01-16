9dc0d9d20074a292ec8fcae9cb007a09c35b9cb7
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2023-01-16 14:48:09 +0100

    Code style error messages

--- credentials.go
+++ credentials.go
@@ -30,7 +30,7 @@ func getJWTToken(accessToken string, issuerURL string) (string, error) {
 
 	if resp != nil && resp.StatusCode != 201 {
 		log.Warn("Failed in fetching JWT Token, responsecode: ", resp.StatusCode)
-		return "", errors.New("Failed in fetching JWT Token")
+		return "", errors.New("failed in fetching JWT Token")
 	}
 
 	return jwt.Token, nil
@@ -49,7 +49,7 @@ func getCACert(issuerURL string) ([]byte, error) {
 
 	if resp != nil && resp.StatusCode != 200 {
 		log.Warn("Failed in fetching CA certificate, responsecode: ", resp.StatusCode)
-		return nil, errors.New("Failed in fetching CA certificate")
+		return nil, errors.New("failed in fetching CA certificate")
 	}
 	return []byte(caInstance.Cert), nil
 }
